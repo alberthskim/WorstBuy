06e236b8e6a5107b000d64165d79e1d552565f81
Albert <Alberthsk115@gmail.com>
2023-06-18 22:27:50 -0700

    finished log-in page

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -3,36 +3,50 @@ import { login } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect, Link } from "react-router-dom";
 import "./LoginForm.css";
+import { useHistory } from "react-router-dom";
 
 
 function LoginFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/" />;
+  // if (sessionUser) return <Redirect to="/" />;
+  // if (sessionUser) history.goBack();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
     if (data) {
-      setErrors(data);
+      const signInErrors = {};
+      for (let error of data) {
+        if (error.startsWith("email")) {
+          signInErrors.email = error.slice(8);
+        } else if (error.startsWith("password")) {
+          signInErrors.password = error.slice(11);
+        }
+      }
+      setErrors(signInErrors);
+      return;
     }
+    history.goBack()
+  };
+
+  const demoUser = async (e) => {
+    e.preventDefault();
+    await dispatch(login("demo@aa.io", "password"));
+    history.goBack();
   };
 
   return (
     <div className="sign-up-page-whole">
       <div className="form-container">
-        <h3 className="sign-in-header">Sign In To Worst Buy</h3>
+        <h2 className="sign-in-header">Sign In To Worst Buy</h2>
         <div className="form-area">
           <form onSubmit={handleSubmit}>
-            {/* <ul>
-            {errors.map((error, idx) => (
-              <li key={idx}>{error}</li>
-            ))}
-          </ul> */}
             <div className="form-detail">
               <label className="input-box">
                 <input
@@ -43,6 +57,9 @@ function LoginFormPage() {
                   onChange={(e) => setEmail(e.target.value)}
                   required
                 />
+                {errors.email && (
+                  <p className="sign-in-errors">{errors.email}</p>
+                )}
               </label>
               <label className="input-box">
                 <input
@@ -53,6 +70,9 @@ function LoginFormPage() {
                   onChange={(e) => setPassword(e.target.value)}
                   required
                 />
+              {errors.password && (
+                  <p className="sign-in-errors">{errors.password}</p>
+              )}
               </label>
               <div className="button-area">
                 <button type="submit" className="sign-in-button">
@@ -65,6 +85,9 @@ function LoginFormPage() {
             </div>
             <div className="create-acc">
               <h4>Don't have an account? <Link className="sign-up-page" to='/signup'>Create an account</Link></h4>
+            </div>
+            <div onClick={demoUser} className="demo-card">
+                <button className="demo-name">Demo User</button>
             </div>
           </form>
         </div>
