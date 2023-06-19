b1978a222965f27f67bea306773d8c776e635eb8
Albert <Alberthsk115@gmail.com>
2023-06-19 16:32:22 -0700

    sign-up form done and validations added

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -67,16 +67,18 @@ export const logout = () => async (dispatch) => {
 	}
 };
 
-export const signUp = (username, email, password) => async (dispatch) => {
+export const signUp = (first_name, last_name, email, password, phone_number) => async (dispatch) => {
 	const response = await fetch("/api/auth/signup", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({
-			username,
+			first_name,
+			last_name,
 			email,
 			password,
+			phone_number
 		}),
 	});
 
