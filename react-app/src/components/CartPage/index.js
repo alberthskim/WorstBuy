f5b5502f2d36a2cb70623e2f015d2c7f46ec478b
Albert <Alberthsk115@gmail.com>
2023-06-23 12:23:21 -0700

    cart-page css

--- react-app/src/components/CartPage/index.js
+++ react-app/src/components/CartPage/index.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import './cartpage.css'
 import { allCartItemsThunk, deleteAllCartThunk, deleteCartItemThunk, updateCartItemThunk } from '../../store/cart'
 import React, { useEffect} from 'react'
-import {useHistory} from 'react-router-dom'
+import {useHistory, Link} from 'react-router-dom'
 
 function CartPage() {
     const dispatch = useDispatch()
@@ -39,19 +39,20 @@ function CartPage() {
     return (
         <div className="cart-page">
 
-
             <div className="cart-left-side-container">
-
-                <div className="cart-top-area">
-                    <div className="cart-items-stuff">
-                        {cartItems.map((item) => (
-                            <>
+                {cartItems.length ? (
+                    <div className="cart-top-area">
+                        <h2>Your Cart</h2>
+                        <div className="cart-items-stuff">
+                            {cartItems.map((item) => (
                                 <div className="individual-item">
-                                    <img className="cart-item-image" src={item.productImage} alt="item-img"/>
-                                    <p>{item.productName}</p>
+                                    <div className="image-name">
+                                        <img className="cart-item-image" src={item.productImage} alt="item-img"/>
+                                        <h4 className="cart-product-name">{item.productName}</h4>
+                                    </div>
                                     <div className="quantity-price">
                                         <div className="quantity-select">
-                                            <select value={item.quantity} onChange={(e) => quantityChange(item.productId, e.target.value)}>
+                                            <select className="select-option" value={item.quantity} onChange={(e) => quantityChange(item.productId, e.target.value)}>
                                                 <option value="1">1</option>
                                                 <option value="2">2</option>
                                                 <option value="3">3</option>
@@ -65,62 +66,91 @@ function CartPage() {
                                             </select>
                                             <p className="remove-item" onClick={() => dispatch(deleteCartItemThunk(item.id, user.id, item.productId))}>remove</p>
                                         </div>
-                                        <p>${item.productPrice}</p>
+                                        <div className="pricing-area">
+                                            <p className="single-amount-times">${(item.productPrice * Number(item.quantity)).toFixed(2)}</p>
+                                            {item.quantity > 1 ? <p className="single-amount-price">(${item.productPrice} each)</p> : null}
+                                        </div>
                                     </div>
                                 </div>
-                            </>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                </div>
+
+                ) : (
+                    <div className="cart-top-area">
+                        <h2>Your cart is empty</h2>
+                    </div>
+                )}
                 {/* <div className="cart-bottom-area">
                     <p>Saved Items</p>
                 </div> */}
-
-
             </div>
 
 
 
             <div className="right-side">
+                {cartItems.length ? (
+                    <div className="order-summary">
+                        <div className="order-header">
+                            <h2>Order Summary</h2>
+                        </div>
+                        <div className="order-details">
 
-                <div className="order-summary">
-                    <div className="order-header">
-                        <h2>Order Summary</h2>
-                    </div>
-                    <div className="order-details">
+                            <div className="bill">
+                                <p>Original Price</p>
+                                <p>Shipping Price</p>
+                                <p>Estimated Sales Tax</p>
+                            </div>
 
-                        <div className="bill">
-                            <p>Original Price</p>
-                            <p>Shipping Price</p>
-                            <p>Estimated Sales Tax</p>
+                            <div className="price">
+                                <p>$ {totalItemPrice}</p>
+                                <p>FREE</p>
+                                <p>$ {estimatedTax}</p>
+                            </div>
+
                         </div>
 
-                        <div className="price">
-                            <p>$ {totalItemPrice}</p>
-                            <p>FREE</p>
-                            <p>$ {estimatedTax}</p>
+                        <div className="checkout-area">
+
+                            <div className="total">
+                                <h3>Total</h3>
+                            </div>
+                            <div className="total-amount">
+                                <h3>$ {totalPriceAmount}</h3>
+                            </div>
+
+                        </div>
+
+                        <div className="button-area">
+                            <button className="checkout" onClick={() => {
+                                dispatch(deleteAllCartThunk());
+                                alert("Your purchase has been made! Please check your email for the order confirmation number. Thank you for shopping with Worst Buy.")
+                                }}>Checkout</button>
+                        </div>
+                    </div>
+
+                ) : (
+                    <div className="order-summary">
+                        <div className="order-header">
+                            <h2>Order Summary</h2>
+                        </div>
+                        <div className="checkout-area">
+
+                            <div className="total">
+                                <h3>Total</h3>
+                            </div>
+                            <div className="total-amount">
+                                <h3>$ {totalPriceAmount}</h3>
+                            </div>
+                        </div>
+                        <div className="button-area">
+                            <button className="checkout-empty" onClick={() => {
+                                history.push('/products')
+                                }}>Continue Shopping</button>
                         </div>
 
                     </div>
-
-                    <div className="checkout-area">
-
-                        <div className="total">
-                            <p>Total</p>
-                        </div>
-                        <div className="total-amount">
-                            <p>$ {totalPriceAmount}</p>
-                        </div>
-
-                    </div>
-
-                    <div className="button-area">
-                         <button className="checkout" onClick={() => {
-                             dispatch(deleteAllCartThunk());
-                             alert("Your purchase has been made! Please check your email for the order confirmation number. Thank you for shopping with Worst Buy.")
-                            }}>Checkout</button>
-                    </div>
-                </div>
+                )}
 
 
             </div>
